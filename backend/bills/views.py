3ca57e89afaaec3a369e9cc0dc8090fe309cc6dc
TSBrinker <tsbrinker@gmail.com>
2022-12-07 16:11:09 -0600

    PUT status for Bill now only works if the user is in the list of users on the bill itself

--- backend/bills/views.py
+++ backend/bills/views.py
@@ -28,8 +28,13 @@ def user_bills(request):
 @permission_classes([IsAuthenticated])
 def update_bill(request, pk):
     bill = get_object_or_404(Bill, pk=pk)
-    if request.method == 'PUT':
-        serializer = BillSerializer(bill, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+    print(f"request.data: [{request.data}]")
+    print(f"request.user: {request.user}")
+    if request.user.id in request.data["users"]:
+        if request.method == 'PUT':
+            serializer = BillSerializer(bill, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
