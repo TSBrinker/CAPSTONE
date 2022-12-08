26f7cae1974c185d7450145abe9d914229a10d3e
TSBrinker <tsbrinker@gmail.com>
2022-12-08 14:08:34 -0600

    added PUT and DEL requests for Categories app

--- backend/categories/serializers.py
+++ backend/categories/serializers.py
@@ -4,5 +4,5 @@ from .models import Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'user', 'name', 'description']
+        fields = ['id', 'name', 'description', 'user']
         depth = 1
