aaea098e3f443eb715637863344c353f97b5a30a
TSBrinker <tsbrinker@gmail.com>
2022-12-08 13:06:52 -0600

    Added update and delete by id requests for products

--- backend/products/views.py
+++ backend/products/views.py
@@ -21,3 +21,16 @@ def get_products(request):
         products = Product.objects.filter(users__id=request.user.id)
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
+@api_view(['PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def update_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
