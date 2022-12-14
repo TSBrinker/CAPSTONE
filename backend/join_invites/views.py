f23db1ff28f87d462c87b69eccd56c57df6b66bf
TSBrinker <tsbrinker@gmail.com>
2022-12-14 15:29:56 -0600

    Added functionality to accept an invite and delete an invite, as well as pull invite by the invite number

--- backend/join_invites/views.py
+++ backend/join_invites/views.py
@@ -6,6 +6,7 @@ from .models import JoinInvite
 from .serializers import JoinInviteSerializer
 from django.shortcuts import get_object_or_404
 from households.models import Household
+from households.serializers import HouseholdSerializer
 from authentication.serializers import RegistrationSerializer
 from authentication.models import User
 import random
@@ -18,10 +19,9 @@ def household_invites(request, hpk):
     household = get_object_or_404(Household, pk=hpk)
     if request.user.is_admin and request.user.household_id == household.id:
         invite_number = str(random.randint(10000000, 99999999))
-        print(f'invite_number: {invite_number}, {type(invite_number)}')
+
         if request.method == 'POST':
             serializer = JoinInviteSerializer(data=request.data)
-            print(serializer.initial_data)
             if serializer.is_valid():
                 serializer.save(invite_number=invite_number, household=household)
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -34,6 +34,25 @@ def household_invites(request, hpk):
             else:
                 return Response(status=status.HTTP_403_FORBIDDEN)
     else:
-        print("User is not admin OR user's household doesn't match the household in the hpk")
         return Response(status=status.HTTP_403_FORBIDDEN)
+
+@api_view(['PATCH', 'DELETE', 'GET'])
+@permission_classes([IsAuthenticated])
+def handle_invite(request, inv_no):
+    invite = get_object_or_404(JoinInvite, invite_number=inv_no)
+    user = get_object_or_404(User, pk=request.user.id)
+    # household = get_object_or_404(Household, pk=invite.household)
+    if request.method == 'PATCH':
+        serializer = RegistrationSerializer(user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(household_id=invite.household_id, is_admin=False)
+        invite.delete()
+        return Response(status=status.HTTP_202_ACCEPTED)
+    elif request.method == 'DELETE':
+        invite.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'GET':
+        serializer = JoinInviteSerializer(invite)
+        return Response(serializer.data)
+
 
